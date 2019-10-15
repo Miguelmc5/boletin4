5ff91661bce9831bb5d9d739bffd0609c096d756
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-15 11:00:55 +0200

    acabarlo

---
+++ boletin4_1/Boletin4_1.java
@@ -0,0 +1,22 @@
+package boletin4_1;
+
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class Boletin4_1 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    Coche cochito= new Coche();
+    cochito.mostrar();
+     cochito.setaceleracion(10);
+    Coche cochito2= new Coche(20,30);
+   int valor=cochito2.
+
+    
+    }
+    
+}
