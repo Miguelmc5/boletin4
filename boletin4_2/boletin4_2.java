4ef02ded12429d5f66bf1e819dfb43963679fd00
Miguel Martinez Costa <56074037+Miguelmc5@users.noreply.github.com>
2019-10-16 17:34:04 +0200

    Add files via upload

---
+++ boletin4_2/boletin4_2.java
@@ -0,0 +1,18 @@
+package boletin4_2;
+
+/**
+ *
+ * @author usuario
+ */
+public class boletin4_2 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    Satelite sat= new Satelite();
+    sat.verPosicion();    
+    
+    }
+    
+}
