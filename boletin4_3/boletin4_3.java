19a84b08172995f7f5eac1393fb08429a1d4c229
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-16 17:46:57 +0200

    clase main

---
+++ boletin4_3/boletin4_3.java
@@ -0,0 +1,16 @@
+package boletin4_3;
+
+/**
+ *
+ * @author usuario
+ */
+public class boletin4_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
